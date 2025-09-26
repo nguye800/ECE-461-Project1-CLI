504ac041ba65a8d59453cd0c6f36499bd4c7eda2
esong21 <96155582+esong21@users.noreply.github.com>
2025-09-25 23:41:10 -0400

    Code for RampUpTime and Size, created test_metric folder

--- src/classes/BusFactor.py
+++ src/classes/BusFactor.py
@@ -12,7 +12,7 @@ class BusFactor(Metric):
 
     def setNumContributors(self, url):
         avg, std, authors = getCollaborators(url, n=200)
-        evenness = 1.0 / (1.0 + (std / avg)**2) # rewards balanced contribution, penalizes concentration
+        evenness = 1.0 / (1.0 + (std / avg) **2) # rewards balanced contribution, penalizes concentration
         saturation_coeff = 5
         groupsize = 1 - math.exp((-1.0 / avg) / saturation_coeff)
         self.NumContributors = evenness * groupsize
